b4afac52bcb37d9a9c39841049cc0e56206ef265
yuviputra <yuviputra@gmail.com>
2020-04-18 21:39:46 +0700

    tugas1 2 3 4

---
+++ tugas4.js
@@ -0,0 +1,20 @@
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondWord4 = word4.substring(4,14); 
+var thirdWord4 = word4.substring(15,17);
+var fourthWord4 = word4.substring(18,20);
+var fifthWord4 = word4.substring(21,25);
+
+
+var firstWord4Length = exampleFirstWord4.length;
+// create new variables around here
+var secondWord4Length = secondWord4.length;
+var thirdWord4Length = thirdWord4.length;
+var fourthWord4Length = fourthWord4.length;
+var fifthWord4Length = fifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWord4Length);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWord4Length);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWord4Length);
+console.log('Fourth Word: ' + fourthWord4 + ',with length: ' + fourthWord4Length);
+console.log('Fifth Word: ' + fifthWord4 + ',with length: ' + fifthWord4Length);
